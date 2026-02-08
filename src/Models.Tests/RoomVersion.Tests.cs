d0b052aaab3e947ddc194216249a90006bf3026e
Nikita Tulenev <n1tulenev@gmail.com>
2026-02-08 07:51:12 +0100

    Add RoomVersion Tests

---
+++ src/Models.Tests/RoomVersion.Tests.cs
@@ -0,0 +1,46 @@
+using FluentAssertions;
+
+namespace Models.Tests;
+
+public sealed class RoomVersionTests
+{
+    [Fact(DisplayName = "CtorShouldSetValue")]
+    [Trait("Category", "Unit")]
+    public void CtorShouldSetValue()
+    {
+        var version = new RoomVersion(2);
+
+        version.Value.Should().Be(2);
+    }
+
+    [Theory(DisplayName = "CtorShouldThrowWhenValueIsLessThanOne")]
+    [Trait("Category", "Unit")]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void CtorShouldThrowWhenValueIsLessThanOne(int value)
+    {
+        var action = () => new RoomVersion(value);
+
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact(DisplayName = "NextShouldReturnNextVersion")]
+    [Trait("Category", "Unit")]
+    public void NextShouldReturnNextVersion()
+    {
+        var version = new RoomVersion(1);
+
+        var next = version.Next();
+
+        next.Value.Should().Be(2);
+    }
+
+    [Fact(DisplayName = "ToStringShouldReturnValue")]
+    [Trait("Category", "Unit")]
+    public void ToStringShouldReturnValue()
+    {
+        var version = new RoomVersion(12);
+
+        version.ToString().Should().Be("12");
+    }
+}
