1fd0ca254bd9e50b7993a7a659b8376be6d99113
Nikita Tulenev <n1tulenev@gmail.com>
2026-02-11 16:22:11 +0100

    Fix formatting

--- src/SealCode.Tests/AdminUserManager.Tests.cs
+++ src/SealCode.Tests/AdminUserManager.Tests.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Options;
+
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
